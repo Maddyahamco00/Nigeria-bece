95d19e150548ddc34af9c3b41156d6f9e1b2b8ac
Maddyahamco00 <maddyahamco00@gmail.com>
2025-08-11 13:52:12 +0100

    Update passport.js

--- Nigeria bece/config/passport.js
+++ Nigeria bece/config/passport.js
@@ -1,41 +1,43 @@
+// config/passport.js
+
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
-const pool = require('./database');
+const User = require('../models/User');
 
-module.exports = function(passport) {
+module.exports = passport => {
+  // Local strategy
   passport.use(
-    new LocalStrategy(
-      {
-        usernameField: 'email',
-        passwordField: 'password'
-      },
-      async (email, password, done) => {
-        try {
-          const [users] = await pool.query('SELECT * FROM users WHERE email = ?', [email]);
-          if (!users.length) {
-            return done(null, false, { message: 'Incorrect email.' });
-          }
-          const user = users[0];
-          const isMatch = await bcrypt.compare(password, user.password);
-          if (!isMatch) {
-            return done(null, false, { message: 'Incorrect password.' });
-          }
-          return done(null, user);
-        } catch (err) {
-          return done(err);
+    new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+      try {
+        // Check if user exists
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+          return done(null, false, { message: 'Email not registered' });
         }
+
+        // Match password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
       }
-    )
+    })
   );
 
+  // Serialize user
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
+  // Deserialize user
   passport.deserializeUser(async (id, done) => {
     try {
-      const [users] = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
-      done(null, users[0]);
+      const user = await User.findByPk(id);
+      done(null, user);
     } catch (err) {
       done(err);
     }
